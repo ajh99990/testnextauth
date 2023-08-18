e169dda702bcd0c6cf5526723b8b550faac83cc5
ajh99990 <nduonduo@qq.com>
2023-08-18 10:33:56 +0800

    1

--- src/app/mine/page.tsx
+++ src/app/mine/page.tsx
@@ -1,8 +1,11 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { useSession,signOut } from "next-auth/react";
 
 export default function Mine() {
   const session = useSession();
   console.log(session);
-  return <div>my account name:{session.data?.user?.name}</div>;
+  return (<>
+  <div>my account name:{session.data?.user?.name}</div>
+  <button onClick={()=>{signOut({ callbackUrl: '/login' })}}>logout</button>
+  </>);
 }
